3015af38f5c972dcb13ff14e8c1ae12b846a7deb
fujisakiyuu <yurayurataro@icloud.com>
2021-02-18 10:35:19 +0900

    修正２

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -53,7 +53,6 @@ class PrototypesController < ApplicationController
   
 
 
-
   private
   def prototype_params 
     params.require(:prototype).permit(:title, :catch_copy,:concept,:image).merge(user_id: current_user.id)
